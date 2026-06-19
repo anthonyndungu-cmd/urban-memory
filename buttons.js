72944ad9f2b9bea2ace5d547f746d1f9ad344bc4
Anthony Ndung'u <anthony.ndungu@strathmore.edu>
2026-06-19 21:17:43 +0300

    add image for one profile

--- buttons.js
+++ buttons.js
@@ -2,5 +2,9 @@ const myButton = document.createElement('button');
 myButton.innerText = 'Show more';
 myButton.className = 'artist-buttton'
 
+myButton.addEventListener('click', function() {
+    location.href = 'artist.html';
+});
+
 const container = document.getElementById('button-container');
 container.appendChild(myButton);
