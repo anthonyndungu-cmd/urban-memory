6179a4ae25cbc572362f51fb77fef6a96d6261b4
Anthony Ndung'u <anthony.ndungu@strathmore.edu>
2026-06-19 20:54:45 +0300

    add button and text

---
+++ buttons.js
@@ -0,0 +1,6 @@
+const myButton = document.createElement('button');
+myButton.innerText = 'Show more';
+myButton.className = 'artist-buttton'
+
+const container = document.getElementById('button-container');
+container.appendChild(myButton);
