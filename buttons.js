9caad4d56949af1404a37c84bd2da55abe9e687c
Anthony Ndung'u <anthony.ndungu@strathmore.edu>
2026-06-26 13:35:17 +0300

    add style to page

--- buttons.js
+++ buttons.js
@@ -5,8 +5,3 @@ myButton.className = 'artist-buttton'
 myButton.addEventListener('click', function() {
     location.href = 'artist-profile.html';
 });
-
-const container = document.getElementById('artist-button');
-container.appendChild(myButton);
-
-const  
