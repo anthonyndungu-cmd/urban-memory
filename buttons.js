2b0cd21fb74f27d721957f14b01806defe3863e3
Anthony Ndung'u <anthony.ndungu@strathmore.edu>
2026-06-28 14:52:04 +0300

    add artist-profile button

--- buttons.js
+++ buttons.js
@@ -1,5 +1,15 @@
 const myButton = document.getElementById('artist-button');
- 
-myButton.addEventListener('click', function() {
-  location.href = 'artist.html';
-});
+
+if (myButton) {
+  myButton.addEventListener('click', function() {
+    location.href = 'artist.html';
+  });
+}
+
+const artistBtn = document.getElementById('profile-button');
+
+if (artistBtn) {
+  artistBtn.addEventListener('click', function() {
+    location.href = 'artist-profile.html';
+  });
+}
